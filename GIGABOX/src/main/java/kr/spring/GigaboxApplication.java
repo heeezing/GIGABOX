f9ebcca5e96059e858caf8312667367075f244fd
heejin <heejin@DESKTOP-OMEHF4K>
2023-07-27 15:09:00 +0900

    first up

---
+++ GIGABOX/src/main/java/kr/spring/GigaboxApplication.java
@@ -0,0 +1,13 @@
+package kr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigaboxApplication.class, args);
+	}
+
+}
