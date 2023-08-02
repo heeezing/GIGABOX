9ffc79d98b2dff7c29152001899ae1c6e17b858c
wise0 <wise0@192.168.0.35>
2023-08-02 16:53:50 +0900

    HallService

---
+++ GIGABOX/src/main/java/kr/spring/hall/service/HallService.java
@@ -0,0 +1,24 @@
+package kr.spring.hall.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.spring.hall.vo.HallVO;
+import kr.spring.theater.vo.TheaterVO;
+
+public interface HallService {
+	// 상영관 등록
+	public void insertHall(HallVO hall);
+	// 상영관 목록
+	public List<HallVO> selectList(Map<String,Object> map);
+	public int selectRowCount(Map<String,Object> map);
+	// 상영관 선택
+	public HallVO selectHall(Integer hall_num);
+	// 상영관 수정
+	public void updateHall(HallVO hall);
+	// 상영관 삭제
+	public void deleteHall(Integer hall_num);
+	
+	// 극장 목록 불러오기
+	public List<TheaterVO> getTheaterList();
+}
